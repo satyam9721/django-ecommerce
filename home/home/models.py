a40f67fe48e9858ff8d0890ca1cc63ac6ccfeee1
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-12-08 12:05:36 +0530

    Add files via upload
    added

---
+++ home/home/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=20, unique=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_date = models.DateField()
+    address = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            last_order = Order.objects.order_by('-id').first()
+            if last_order:
+                last_order_number = int(last_order.order_number[3:])
+                self.order_number = f'ORD{str(last_order_number + 1).zfill(5)}'
+            else:
+                self.order_number = 'ORD00001'
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in {self.order.order_number}"
---
+++ home/home/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+from .models import Customer, Product, Order, OrderItem
+from .serializers import CustomerSerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
